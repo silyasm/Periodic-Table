5b0a6adb42d54d09e7f606cb5b61cbd06d56e5e7
Ilyas Munzir <ilyazmunzir@gmail.com>
2021-02-25 22:22:05 -0600

    Initial Commit

---
+++ Periodic Table/Periodic_TableApp.swift
@@ -0,0 +1,17 @@
+//
+//  Periodic_TableApp.swift
+//  Periodic Table
+//
+//  Created by Student on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Periodic_TableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
