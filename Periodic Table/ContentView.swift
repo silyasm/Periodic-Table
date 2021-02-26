267282a199ba7144601266a4cc782330cbc3cfe3
Ilyas Munzir <ilyazmunzir@gmail.com>
2021-02-25 23:09:49 -0600

    code is completed

--- Periodic Table/ContentView.swift
+++ Periodic Table/ContentView.swift
@@ -8,9 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var elements = [Element]()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(elements) { element in
+                NavigationLink(
+                    destination: VStack {
+                        Text(element.symbol)
+                            .padding()
+                        Text(element.name)
+                            .padding()
+                        Text(element.atomicNumber)
+                            .padding()
+                        Text(element.history)
+                            .paddding()
+                    },
+                    label: {
+                        HStack {
+                            Text(element.symbol)
+                            Text(element.name)
+                        }
+                    })
+            }
+            .navigationTitle("Periodic Table")
+        }
+        .onAppear(perform: {
+            queryAPI()
+        })
+    }
+    func queryAPI() {
+        let apiKey = "53c34bdf56msha93492520b56cffp1c92cajsn116ab7eb6ea6"
+        let query = "https://periodictable.p.rapidapi.com/?rapidapi-key=\(apiKey)"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                let contents = json.arrayValue
+                for item in contents {
+                    let symbol = item["symbol"].stringValue
+                    let name = item["name"].stringValue
+                    let atomicNumber = item["atomic number"].stringValue
+                    let history = item["history"].stringValue
+                    let element = Element(symbol: symbol, name: name, atomicNumber: atomicNumber, history: history)
+                    elements.append(element)
+                }
+            }
+        }
     }
 }
 
@@ -18,4 +60,12 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Element: Identifiable {
+    let id = UUID()
+    let symbol: String
+    let name: String
+    let atomicNumber: String
+    let history: String
 }
